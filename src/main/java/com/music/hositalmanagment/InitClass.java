8b5398662230b883c8f03620298538cf6168711f
Atul Mouraya <atul.mouraya94@gmail.com>
2022-05-28 17:09:51 +0530

    Initial Commit

---
+++ src/main/java/com/music/hositalmanagment/InitClass.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.music.hositalmanagment;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ATul
+ */
+public class InitClass {
+    
+    public static void main(String[] args)
+    {
+       Login lg= new Login();
+       lg.setVisible(true);
+       
+    }
+    
+}
